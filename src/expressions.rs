251b58eba753eb93e2548bab6a6946444b8c262f
Simone <mail@mone27.net>
2024-12-29 21:33:54 +0100

    ♻️ rust features to support operations on units + rename to quantities

--- src/expressions.rs
+++ src/expressions.rs
@@ -7,22 +7,23 @@ use polars::prelude::*;
 use pyo3_polars::derive::polars_expr;
 
 use crate::expressions::polars_plan::prelude::Expr;
+use crate::units::*;
 
-fn check_valid_unit_dtype(dtype: &DataType) -> PolarsResult<bool> {
+fn check_valid_quantity_dtype(dtype: &DataType) -> PolarsResult<bool> {
     match dtype {
         DataType::Struct(fields) => {
             if let (Some(value_field), Some(unit_field)) = (fields.get(0), fields.get(1)) {
+                Units::check_valid_unit_dtype(&unit_field.dtype)?;
                 if value_field.name == "value"
                     && value_field.dtype.is_numeric()
                     && unit_field.name == "unit"
-                    && unit_field.dtype == DataType::String
                 {
                     Ok(true)
                 } else {
-                    polars_bail!(InvalidOperation: "Invalid Unit. Expected struct with fields 'value' and 'unit' and types numeric and String, got {:?}", fields)
+                    polars_bail!(InvalidOperation: "Invalid Quantity. Expected struct with fields 'value' and 'unit' and types numeric and Unit, got {:?}", fields)
                 }
             } else {
-                polars_bail!(InvalidOperation: "Invalid Unit. Expected struct with 2 fields ('value' and 'unit'), got {:?} fields ({:?})", fields.len(), fields.into_iter().map(|f| f.name.clone()).collect::<Vec<_>>())
+                polars_bail!(InvalidOperation: "Invalid Quanity. Expected struct with 2 fields ('value' and 'unit'), got {:?} fields ({:?})", fields.len(), fields.into_iter().map(|f| f.name.clone()).collect::<Vec<_>>())
             }
         },
         dtype => polars_bail!(InvalidOperation: "Expected Struct dtype, got {}", dtype),
@@ -33,14 +34,14 @@ fn is_all_same<T: PartialEq>(slice: &[T]) -> bool {
     slice.windows(2).all(|w| w[0] == w[1])
 }
 
-fn unit_output(input_fields: &[Field]) -> PolarsResult<Field> {
+fn quantity_output(input_fields: &[Field]) -> PolarsResult<Field> {
     let dtypes: Vec<&DataType> = input_fields.iter().map(|f| f.dtype()).collect();
     // simplying assumptions: the value dtype must be the same for all inputs
     if !is_all_same(&dtypes) {
         polars_bail!(InvalidOperation: "Expected all input fields to have the same dtype, got {:?}", dtypes)
     }
     // check that datatype is valid
-    check_valid_unit_dtype(dtypes[0])?;
+    check_valid_quantity_dtype(dtypes[0])?;
     Ok(Field::new(
         "unit".into(),
         DataType::Struct(vec![
@@ -50,25 +51,25 @@ fn unit_output(input_fields: &[Field]) -> PolarsResult<Field> {
     ))
 }
 
-fn check_same_unit(ca: &StringChunked) -> PolarsResult<()> {
+fn check_same_unit(ca: &ListChunked) -> PolarsResult<()> {
     let mut iter = ca.iter();
     let first = iter.next().unwrap();
     if iter.all(|s| s == first) {
         Ok(())
     } else {
-        polars_bail!(InvalidOperation: "Expected all units to be the same, got {:?}", ca.unique())
+        polars_bail!(InvalidOperation: "Expected all units to be the same")
     }
 }
 
-fn extract_unit(input: &Series) -> PolarsResult<(Series, Series)> {
+fn extract_quantity(input: &Series) -> PolarsResult<(Series, Series)> {
     let ca = input.struct_()?;
-    check_valid_unit_dtype(ca.dtype())?;
+    check_valid_quantity_dtype(ca.dtype())?;
     let fields = &ca.fields_as_series();
     let (value, unit) = (
         fields.get(0).unwrap().clone(),
         fields.get(1).unwrap().clone(),
     );
-    check_same_unit(unit.str()?)?;
+    check_same_unit(unit.list()?)?;
 
     Ok((value, unit))
 }
@@ -90,15 +91,20 @@ fn extract_result(df: DataFrame) -> Series {
     return result;
 }
 fn apply_unary(input: &Series, expr: Expr) -> PolarsResult<Series> {
-    let (value, unit) = extract_unit(input)?;
+    let (value, unit) = extract_quantity(input)?;
     let df = df!["value" => value]?.lazy().select(&[expr]).collect()?;
     let result = extract_result(df);
     return add_unit(result, unit.first());
 }
 
-fn apply_binary(left: &Series, right: &Series, expr: Expr) -> PolarsResult<Series> {
-    let (value_left, unit_left) = extract_unit(left)?;
-    let (value_right, unit_right) = extract_unit(right)?;
+fn apply_binary(
+    left: &Series,
+    right: &Series,
+    expr: Expr,
+    unit_tfms: Option<fn(Units, Units) -> Units>,
+) -> PolarsResult<Series> {
+    let (value_left, unit_left) = extract_quantity(left)?;
+    let (value_right, unit_right) = extract_quantity(right)?;
     // for now support operations only if the units are the same
     if unit_left.first() != unit_right.first() {
         polars_bail!(InvalidOperation: "Expected units to be the same, got {:?} and {:?}", unit_left.first(), unit_right.first())
@@ -108,12 +114,21 @@ fn apply_binary(left: &Series, right: &Series, expr: Expr) -> PolarsResult<Serie
         .select(&[expr])
         .collect()?;
     let result = extract_result(df);
-    return add_unit(result, unit_left.first());
+    let new_unit = if let Some(tfms) = unit_tfms {
+        tfms(
+            Units::from_scalar(unit_left.first())?,
+            Units::from_scalar(unit_right.first())?,
+        )
+        .to_scalar()?
+    } else {
+        unit_left.first()
+    };
+    return add_unit(result, new_unit);
 }
 
 macro_rules! create_unit_unary_expr {
     ($name:ident $(, $arg:expr)*) => {
-        #[polars_expr(output_type_func=unit_output)]
+        #[polars_expr(output_type_func=quantity_output)]
         fn $name(inputs: &[Series]) -> PolarsResult<Series> {
             apply_unary(&inputs[0], col("value").$name($($arg),*).alias("result"))
         }
@@ -122,26 +137,33 @@ macro_rules! create_unit_unary_expr {
 
 macro_rules! create_unit_binary_expr {
     ($name:ident) => {
-        #[polars_expr(output_type_func=unit_output)]
+        #[polars_expr(output_type_func=quantity_output)]
         fn $name(inputs: &[Series]) -> PolarsResult<Series> {
             apply_binary(
                 &inputs[0],
                 &inputs[1],
                 col("value_left").$name(col("value_right")).alias("result"),
+                None,
+            )
+        }
+    };
+    ($name:ident, $tfms_unit:expr) => {
+        #[polars_expr(output_type_func=quantity_output)]
+        fn $name(inputs: &[Series]) -> PolarsResult<Series> {
+            apply_binary(
+                &inputs[0],
+                &inputs[1],
+                col("value_left").$name(col("value_right")).alias("result"),
+                Some($tfms_unit),
             )
         }
     };
 }
 
-#[polars_expr(output_type_func=unit_output)]
+#[polars_expr(output_type_func=quantity_output)]
 fn noop(inputs: &[Series]) -> PolarsResult<Series> {
     apply_unary(&inputs[0], col("value").alias("result"))
 }
-
-// #[polars_expr(output_type_func=unit_output)]
-// fn abs(inputs: &[Series]) -> PolarsResult<Series> {
-//     apply_unary(&inputs[0], col("value").abs())
-// }
 
 create_unit_unary_expr!(abs);
 create_unit_binary_expr!(add);
@@ -165,7 +187,7 @@ create_unit_unary_expr!(cum_min, false);
 create_unit_unary_expr!(cum_prod, false);
 // create_unit_unary_expr!(diff);
 
-create_unit_binary_expr!(dot);
+// create_unit_binary_expr!(dot);
 create_unit_unary_expr!(neg);
 // create_unit_unary_expr!(exp);
 // create_unit_unary_expr!(expm1);
@@ -181,8 +203,8 @@ create_unit_unary_expr!(tan);
 create_unit_unary_expr!(tanh);
 create_unit_binary_expr!(sub);
 create_unit_unary_expr!(sin);
-create_unit_binary_expr!(mul);
-create_unit_binary_expr!(div);
+create_unit_binary_expr!(mul, |a, b| a.multiply(&b));
+create_unit_binary_expr!(div, |a, b| a.divide(&b));
 
 create_unit_unary_expr!(min);
 create_unit_unary_expr!(max);
--- src/units.rs
+++ src/units.rs
@@ -1,35 +1,15 @@
 use polars::prelude::*;
 
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Unit {
     name: std::string::String,
     power: i16,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Units {
     units: Vec<Unit>,
 }
-
-// impl Unit {
-//     pub fn from_struct(x: AnyValue) -> PolarsResult<Self> {
-//         if let AnyValue::StructOwned(str_owned) = x {
-//             let (values, fields) = *str_owned;
-//             Self::from_unit_values(&values)
-//         } else {
-//             polars_bail!(InvalidOperation: "Expected StructOwned, got {:?}", x)
-//         }
-//     }
-
-//     fn from_unit_values(values: &[AnyValue]) -> PolarsResult<Unit> {
-//         if let (AnyValue::String(name), AnyValue::Int16(power)) = (&values[0], &values[1]) {
-//             Ok(Unit {
-//                 name: name.to_string(),
-//                 power: *power,
-//             })
-//         } else {
-//             polars_bail!(ComputeError: "Invalid Unit values. Expected 'name' as String and 'power' as Int16, got {:?}", values);
-//         }
-//     }
-// }
 
 impl Units {
     pub fn from_scalar(scalar: Scalar) -> PolarsResult<Self> {
@@ -55,6 +35,22 @@ impl Units {
             polars_bail!(ComputeError: "Expected List of Structs, got {:?}", scalar.dtype());
         }
     }
+
+    pub fn check_valid_unit_dtype(dtype: &DataType) -> PolarsResult<()> {
+        if let DataType::List(inner) = dtype {
+            let inner = inner.as_ref();
+            if let DataType::Struct(fields) = inner.as_ref() {
+                Self::check_unit_fields(&fields)
+            }
+            else {
+                polars_bail!(ComputeError: "Invalid Unit dtype: expected List of Struct, got List of {:?}", inner)
+            }
+        }
+        else {
+            polars_bail!(ComputeError: "Invalid Unit dtype: expected List, got {:?}", dtype)
+        }
+    }
+
     fn check_unit_fields(fields: &[Field]) -> PolarsResult<()> {
         if fields.len() != 2 {
             polars_bail!(ComputeError: "Unit struct must have 2 fields, got {:?}", fields.len());
@@ -70,12 +66,18 @@ impl Units {
         }
         polars_bail!(ComputeError: "Invalid Unit struct. Expected fields 'name' and 'power' with types String and Int16, got {:?}", fields);
     }
-
     pub fn to_scalar(&self) -> PolarsResult<Scalar> {
         let names: Series = self.units.iter().map(|u| u.name.clone()).collect();
         let powers: Series = self.units.iter().map(|u| Some(u.power)).collect();
-        let ca_struct =
-            StructChunked::from_series("unit".into(), names.len(), [names, powers].iter())?;
+        let ca_struct = StructChunked::from_series(
+            "unit".into(),
+            names.len(),
+            [
+                names.with_name("name".into()),
+                powers.with_name("power".into()),
+            ]
+            .iter(),
+        )?;
         Ok(Scalar::new(
             DataType::List(Box::new(DataType::Struct(vec![
                 Field::new("name".into(), DataType::String),
@@ -84,11 +86,40 @@ impl Units {
             AnyValue::List(ca_struct.into_series()),
         ))
     }
+
+    pub fn multiply(&self, other: &Self) -> Self {
+        let mut units = self.units.clone();
+        for unit_other in &other.units {
+            if let Some(u) = units.iter_mut().find(|u| u.name == unit_other.name) {
+                u.power += unit_other.power;
+            } else {
+                units.push(unit_other.clone());
+            }
+        }
+        Self { units }
+    }
+
+    pub fn divide(&self, other: &Self) -> Self {
+        let mut units = self.units.clone();
+        for unit_other in &other.units {
+            if let Some(u) = units.iter_mut().find(|u| u.name == unit_other.name) {
+                u.power -= unit_other.power;
+            } else {
+                units.push(Unit {
+                    name: unit_other.name.clone(),
+                    power: -unit_other.power,
+                });
+            }
+        }
+        Self { units }
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
     fn test_to_scalar() {
         let units = Units {
             units: vec![
@@ -103,18 +134,94 @@ mod test {
             ],
         };
         let scalar = units.to_scalar().unwrap();
-        assert_eq!(
-            scalar.dtype(),
-            DataType::List(Box::new(DataType::Struct(vec![
-                Field::new("name".into(), DataType::String),
-                Field::new("power".into(), DataType::Int16),
-            ])))
-        );
         let units = Units::from_scalar(scalar).unwrap();
         assert_eq!(units.units.len(), 2);
         assert_eq!(units.units[0].name, "m");
         assert_eq!(units.units[0].power, 1);
         assert_eq!(units.units[1].name, "s");
         assert_eq!(units.units[1].power, 2);
+    }
+
+    #[test]
+    fn test_empty_units() {
+        let units = Units { units: vec![] };
+        let scalar = units.to_scalar().unwrap();
+        let units = Units::from_scalar(scalar).unwrap();
+        assert_eq!(units.units.len(), 0);
+    }
+
+    #[test]
+    fn test_invalid_scalar() {
+        let invalid_scalar = Scalar::new(DataType::Int32, AnyValue::Int32(42));
+        let result = Units::from_scalar(invalid_scalar);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_multiply() {
+        let units1 = Units {
+            units: vec![
+                Unit {
+                    name: "m".to_string(),
+                    power: 1,
+                },
+                Unit {
+                    name: "s".to_string(),
+                    power: 2,
+                },
+            ],
+        };
+        let units2 = Units {
+            units: vec![
+                Unit {
+                    name: "m".to_string(),
+                    power: 2,
+                },
+                Unit {
+                    name: "s".to_string(),
+                    power: 3,
+                },
+            ],
+        };
+        let units = units1.multiply(&units2);
+        assert_eq!(units.units.len(), 2);
+        assert_eq!(units.units[0].name, "m");
+        assert_eq!(units.units[0].power, 3);
+        assert_eq!(units.units[1].name, "s");
+        assert_eq!(units.units[1].power, 5);
+    }
+
+    #[test]
+    fn test_divide() {
+        let units1 = Units {
+            units: vec![
+                Unit {
+                    name: "m".to_string(),
+                    power: 1,
+                },
+                Unit {
+                    name: "s".to_string(),
+                    power: 2,
+                },
+            ],
+        };
+        let units2 = Units {
+            units: vec![
+                Unit {
+                    name: "m".to_string(),
+                    power: 2,
+                },
+                Unit {
+                    name: "s".to_string(),
+                    power: 3,
+                },
+            ],
+        };
+        let units = units1.divide(&units2);
+        assert_eq!(units.units.len(), 2);
+        assert_eq!(units.units[0].name, "m");
+        assert_eq!(units.units[0].power, -1);
+        assert_eq!(units.units[1].name, "s");
+        assert_eq!(units.units[1].power, -1);
     }
 }
